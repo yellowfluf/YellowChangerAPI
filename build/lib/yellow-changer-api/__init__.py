8375e415d9e5a73f982ea9c0d584346c65a2c09c
Whom-m0rty <56591372+Whom-m0rty@users.noreply.github.com>
2024-08-04 16:50:03 +0200

    delete not necessary files

--- build/lib/yellow-changer-api/__init__.py
+++
@@ -1,3 +0,0 @@
-from .yellow_changer import YellowChanger
-
-__all__ = ['YellowChanger']
--- build/lib/yellow-changer-api/exceptions.py
+++
@@ -1,8 +0,0 @@
-
-
-class BadRequest(Exception):
-    """Exception raised for invalid requests."""
-
-    def __init__(self, message: str = "Bad request"):
-        self.message = message
-        super().__init__(self.message)
